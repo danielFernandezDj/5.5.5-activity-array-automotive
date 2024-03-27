34012f50e02cb0a5744dc3fab3094b891d63057e
danielFernandezJd <mdanif9728@gmail.com>
2024-03-27 01:34:11 -0700

    small changes

--- README.md
+++ README.md
@@ -6,3 +6,33 @@
 [Official Node.js docs](https://nodejs.org/docs/v0.4.2/api/modules.html#all_Together...)
 
 (NOTE: You'll write your derived Car class in a file you make called index.js.)
+
+
+useGasBrake() {
+        if (this.engine) {
+            for (let i = 0; i < 100 && !this.gas == 0; i++) {
+                this.mileage += 1
+                this.gas -= 1
+
+                if (this.speed < 90) {
+                    this.speed += 2
+                }
+
+                if (this.gas <= 0) {
+                    this.gas = 0
+                }
+
+                if (this.gas === 0) {
+                    console.log(`Empty Gas Tank! \n Engine Off!`);
+                    this.engine = false;
+                }
+
+                console.log(`${this.mileage}ml _ ${this.speed}-mlph _ Gas${this.gas}.gl`)
+
+                if (this.mileage >= 100) {
+                    //? If you want to see this massage, change the value of (this.gas -= '3') to "1"!
+                    console.log(`Your are at your destination!`)
+                }
+            }
+        }
+    }
--- mercurySedan.js
+++ mercurySedan.js
@@ -1,86 +1,65 @@
 //this includes the vehicle class as a module
 const VehicleModule = require("./vehicleBaseClass").Vehicle
 
-//! TODO: I need to learn how to make the code short with "cGPT"!!!
-
 // * My code:
 class mercurySedan extends VehicleModule {
     constructor(mileage) {
         super(mileage)
         this.mileage = 0
         this.engine = false
-        this.gas = 250 // 
+        this.gas = 250
         this.speed = 0
     }
 
-    //! Method: 
+    //? Method: 
     //* When prest the button "Start_Engine" if the "Tank as Gas".
     //* And turn the "Start_Engine" true.
     //* If prest the "Button" and "Is not Gas in the Tank" {console.log ("The Gas Tank is empty!")}
     startEngine() {
         if (this.engine === false && this.gas >= 5) {
             this.engine = true
-            this.gas -= 3
-            console.log(`-START-`)
-            console.log(`rum-rum, Ruunnnnn!`)
-            console.log(`Your Gas Tank is at "${this.gas}.gl"`)
+            this.gas -= 5
+            console.log(`-START- \n rum-rumh, Ruunnnnn! \n Your Gas Tank is at "${this.gas}.gl"`)
         } else {
-            console.log(`The Gas Tank is empty! _ log.gas => ${this.gas}gl _ ${this.mileage}-ml.Ph => return: False`)
+            console.log(`\n The Gas Tank is empty! _ ${this.gas}gl`)
             return false
         }
     }
 
 
-    //! Method: 
-    //* If the "startEngine" is true  
-    //* Make a max of "Tank Gas load" and reduce it wen the "Gas_Brake" are preset, 
-    //* if the "Gas_Brake" is preset turn the speed up, and reduce the "Tank Gas load"
-    //* If is not Gas turn "startEngine" false.
+    //? Method: 
+    ///// * If the "startEngine" is true  
+    ///// * Make a max of "Tank Gas load" and reduce it wen the "Gas_Brake" are preset, 
+    ///// * if the "Gas_Brake" is preset turn the speed up, and reduce the "Tank Gas load"
+    ///// * If is not Gas turn "startEngine" false.
     useGasBrake() {
-        if (this.engine === true) {
-            for (let i = 0; i < 100 && !this.gas == 0; i++) {
-                this.mileage += 1
-                this.gas -= 1
+        if (this.engine) {
+            for (let i = 0; i < 500 && this.gas > 0; i++) {
+                this.mileage++;
+                this.gas--;
+                this.speed = Math.min(this.speed + 2, 90);
+                console.log(`${this.mileage}m _ ${this.speed}-mph _ Gas${this.gas}.gl`);
 
-                if (this.speed < 90) {
-                    this.speed += 2
+                if (this.mileage >= 100) { //! Change to 500 to see the console.lol()!
+                    console.log(`Your are at your destination!`);
+                    break; // Stop the loop once destination reached
                 }
-
-                if (this.gas <= 0) {
-                    this.gas = 0
-                }
-
-                if (this.gas == 0) {
-                    console.log(`Empty Gas Tank!`)
-                    console.log(`Engine Off!`)
-                    this.engine = false
-                    console.log(this.engine) //! Engine is false now!
-                    return
-                }
-
-                console.log(`${this.mileage}ml _ ${this.speed}-mlph _ Gas${this.gas}.gl`)
-
-                if (this.mileage >= 100) {
-                    //? If you want to see this massage, change the value of (this.gas -= '3') to "1"!
-                    console.log(`Your are at your destination!`)
-                }
+            }
+            if (this.gas === 0) {
+                console.log(`Empty Gas Tank! \n Engine Off!`);
+                this.engine = false;
             }
         }
     }
 
 
-    //! Method:  
-    //* If "this.engine" is true => turn the "this.engine" false.
-    //* If "stopEngine" is true do {consol.log ("Prest the "Button" to Start the Engine")}
+    //? Method:  
+    ///// If "this.engine" is true => turn the "this.engine" false.
+    ///// If "stopEngine" is true do {consol.log ("Prest the "Button" to Start the Engine")}
     stopEngine() {
-        if (this.engine === true && this.gas > 1) {
+        if (this.engine && this.gas > 1) {
             this.engine = false
-            
-            console.log(`-Your Gas Tank is at "${this.gas}.gl"-`)
-            console.log("")
-            console.log("Engine OFF")
-            console.log("Prest the 'Button' to Start the Engine")
-            console.log("")
+            console.log(`\n -Your Gas Tank is at "${this.gas}.gl"- \n Engine OFF \n Prest the 'Button' to Start the Engine \n\n`)
         }
     }
 }
@@ -92,4 +71,6 @@ callSedan.useGasBrake()
 callSedan.stopEngine()
 
 //* Start again:
-callSedan.startEngine()
+// callSedan.startEngine()
+
+
